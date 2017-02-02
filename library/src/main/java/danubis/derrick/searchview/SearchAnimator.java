a683a2599fc63d7a6c0bab40d298d7010f29bd6d
Derrick Luo <derrick.luo@infitell.com>
2017-02-03 10:43:43 +1100

    updated animation to get better performence.

--- library/src/main/java/danubis/derrick/searchview/SearchAnimator.java
+++ library/src/main/java/danubis/derrick/searchview/SearchAnimator.java
@@ -1,5 +1,6 @@
 package danubis.derrick.searchview;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.view.View;
@@ -28,21 +29,69 @@ class SearchAnimator {
         view.setVisibility(View.GONE);
     }
 
-    static void rise(View view, float distance, int duration) {
+    static void rise(final View view, float distance, int duration) {
+
+        view.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
         ObjectAnimator anim1 = ObjectAnimator.ofFloat(view, "elevation", 0f, 20f);
         ObjectAnimator anim2 = ObjectAnimator.ofFloat(view, "y", view.getY(), view.getY() - distance);
 
         AnimatorSet set = new AnimatorSet();
+        set.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                view.setLayerType(View.LAYER_TYPE_NONE, null);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
         set.playTogether(anim1, anim2);
         set.setDuration(duration);
         set.start();
     }
 
-    static void fall(View view, float distance, int duration) {
+    static void fall(final View view, float distance, int duration) {
+
+        view.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
         ObjectAnimator anim1 = ObjectAnimator.ofFloat(view, "elevation", 20f, 0f);
         ObjectAnimator anim2 = ObjectAnimator.ofFloat(view, "y", view.getY(), view.getY() + distance);
 
         AnimatorSet set = new AnimatorSet();
+        set.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                view.setLayerType(View.LAYER_TYPE_NONE, null);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
         set.playTogether(anim1, anim2);
         set.setDuration(duration);
         set.start();
